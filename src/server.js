a5d6402a92a3fc2c8a8b10014d95f3691a61b526
Trung Nguyen <92764631+2trung@users.noreply.github.com>
2024-02-06 20:42:19 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,15 +7,6 @@ const hostname = 'localhost'
 const port = 8080
 
 app.get('/', (req, res) => {
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
   res.end('<h1>Hello World!</h1><hr>')
 })
 
