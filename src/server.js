cda545c278ed36f526933fd0b38a42b2b19b8e71
Trung Nguyen <trung.nguyen@msn.com>
2024-02-07 14:35:26 +0700

    Connet to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,25 +1,42 @@
+/* eslint-disable no-console */
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
+import { CONNECT_DB, GET_DB } from './config/mongodb'
 
-const hostname = 'localhost'
-const port = 8080
 
-app.get('/', (req, res) => {
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+const START_SERVER = () => {
+  const hostname = 'localhost'
+  const port = 8080
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server runing  at http://${ hostname }:${ port }/`)
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server runing  at http://${ hostname }:${ port }/`)
+  })
+}
+
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error),
+//     process.exit(0)
+//   })
+
+(async () => {
+  try {
+    console.log('Connecting to MongoDB...')
+    await CONNECT_DB()
+    console.log('Connected to MongoDB')
+    START_SERVER()  }
+  catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
