6b8dc4dceba4c0d31c7ece7e25b0772b07a834bf
Trung Nguyen <trung.nguyen@msn.com>
2024-02-12 15:57:40 +0700

    Add column/card Api

---
+++ src/controllers/columnController.js
@@ -0,0 +1,12 @@
+import { StatusCodes } from 'http-status-codes'
+import { columnService } from '~/services/columnService'
+
+const createNew = async (req, res, next) => {
+  try {
+    const createNewColumn = await columnService.createNew(req.body)
+    res.status(StatusCodes.CREATED).json(createNewColumn)
+  } catch (error) { next(error) }
+}
+export const columnController = {
+  createNew
+}
